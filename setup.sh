943e1fb7ebbbe67c5a1f06b2894755246308eb53
RZZT <raulsanchezzt@duck.com>
2023-07-04 22:18:54 +0200

    🍄 Update setup script

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Utilities
-sudo apt install -y xfce4 xfce4-goodies xrdp htop vim firefox gparted
+sudo apt install -y xfce4 xfce4-goodies xrdp htop vim firefox gparted avahi-daemon avahi-discover libnss-mdns
 
 # Docker -> https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 sudo apt update -y
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -20,10 +20,8 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo docker run hello-world
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Bashrc Alias
 sudo echo "# Custom Alias" >> /root/.bashrc
@@ -34,3 +32,11 @@ sudo echo "alias dcdown='docker compose down'" >> /root/.bashrc
 # Stop default DNS service
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
+
+# Install Code Server
+cd code-server
+sudo docker compose up -d
+
+# End
+echo "Now your system is ready, edit the .env files of the other services on Code: http://${HOSTNAME}:8443"
+echo "Then, open the script compose.sh and choose the services you want to start."
