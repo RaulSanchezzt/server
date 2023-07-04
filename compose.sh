da758caa4926641b63d17cf50129a75ea44db0a4
RZZT <raulsanchezzt@duck.com>
2023-07-04 22:18:37 +0200

    🎩 Add Compose Script

---
+++ compose.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Default services to install
+services=(
+  # "blockscout" 
+  # "code-server" -> Alredy installed
+  # "ghostfolio" 
+  "netdata" 
+  "nextcloud" 
+  "nginx-proxy-manager" 
+  # "odoo" 
+  "ouroboros" 
+  "pi-hole" 
+  "portainer" 
+  # "traefik" 
+  "truffle" 
+  "vaultwarden" 
+  "wireguard" 
+  # "wordpress"
+  )
+
+for service in ${services[@]}; do
+  if [ -f "$service/docker-compose.yml" ]; then
+    echo "Starting $service"
+    (cd "$service" && docker compose up -d)
+    echo "========================================================"
+  fi
+done
