106d2e9d76b35b910a655fdd875269b0aa504954
RZZT <raulsanchezzt@duck.com>
2023-07-28 19:38:27 +0200

    🔏 Update Trusted Domains script

--- nextcloud/config.sh
+++ nextcloud/config.sh
@@ -3,6 +3,9 @@
 # Default phone region
 sudo docker exec --user www-data -it App php occ config:system:set default_phone_region --value=“ES”
 
+# Add Trusted Domain
+sudo docker exec --user www-data -it App php occ config:system:set trusted_domains 2 --value=cloud.raulsanchezzt.com
+
 # Restart container
 sudo docker restart App
 
