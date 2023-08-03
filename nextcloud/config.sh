532279284d9c7472f8a3bd04c326073c065adf63
RZZT <raulsanchezzt@duck.com>
2023-08-03 16:39:46 +0200

    🧄 Fix commands

--- nextcloud/config.sh
+++ nextcloud/config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Default phone region
-sudo docker exec --user www-data -it App php occ config:system:set default_phone_region --value=“ES”
+sudo docker exec -it App php occ config:system:set default_phone_region --value=“ES”
 
 # Add Trusted Domain
-sudo docker exec --user www-data -it App php occ config:system:set trusted_domains 2 --value=cloud.raulsanchezzt.com
+# sudo docker exec -it App php occ config:system:set trusted_domains 2 --value=cloud.x.com
 
 # Restart container
 sudo docker restart App
