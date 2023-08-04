6061a0873489b2df7905cc28640000ad9d186c15
RZZT <raulsanchezzt@duck.com>
2023-08-05 01:13:01 +0200

    📀 Update volumes

--- nextcloud/config.sh
+++ nextcloud/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default phone region
-sudo docker exec -it App php occ config:system:set default_phone_region --value=“ES”
+sudo docker exec --user www-data -it App php occ config:system:set default_phone_region --value=“ES”
 
 # Add Trusted Domain
 # sudo docker exec -it App php occ config:system:set trusted_domains 2 --value=cloud.x.com
